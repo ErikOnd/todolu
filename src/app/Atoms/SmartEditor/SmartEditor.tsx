7ee9128b58d3c3b6c6ebe78fc239a264cc535ebf
Erik <erik.ondra@yahoo.com>
2025-10-07 07:59:44 +0200

    minor styling adjustments

--- src/app/Atoms/SmartEditor/SmartEditor.tsx
+++ src/app/Atoms/SmartEditor/SmartEditor.tsx
@@ -1,12 +1,22 @@
-import { BlockNoteView } from "@blocknote/mantine";
-import { useCreateBlockNote } from "@blocknote/react";
+import styles from "./SmartEditor.module.scss";
+import {en} from "@blocknote/core/locales";
+import {BlockNoteView} from "@blocknote/mantine";
+import {useCreateBlockNote} from "@blocknote/react";
 import React from "react";
 import "@blocknote/mantine/style.css";
 
 export function SmartEditor() {
-	// Create a new editor instance
-	const editor = useCreateBlockNote();
+	const locale = en;
 
-	// Render the editor
-	return <BlockNoteView editor={editor} />;
+	const editor = useCreateBlockNote({
+		dictionary: {
+			...locale,
+			placeholders: {
+				...locale.placeholders,
+				emptyDocument: "Start typing..",
+			}
+		}
+	});
+
+	return <BlockNoteView editor={editor} className={styles["smart-editor"]} theme="light"/>;
 }
